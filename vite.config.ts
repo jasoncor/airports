822d8cbe24d53f9df6ad2b0698850e9bc22c06f1
Jason Corcoran <jasoncorcoran0306@gmail.com>
2025-06-25 16:06:40 +1000

    fix: turn off auto code splitting

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,10 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "/airports", // Set base for GitHub Pages deployment
+  base: "/airports",
   plugins: [
-    tanstackRouter({ target: "react", autoCodeSplitting: true }),
+    tanstackRouter({ target: "react", autoCodeSplitting: false }),
     react(),
     tailwindcss(),
   ],
