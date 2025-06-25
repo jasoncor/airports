cb8f8e96bb92b05216eece61caaa041977fb4314
Jason Corcoran <jasoncorcoran0306@gmail.com>
2025-06-25 16:21:34 +1000

    fix: hide header on details view

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,14 +1,21 @@
 import { Header } from "./Header";
+import { useRouterState } from "@tanstack/react-router";
 
 export interface PrivateLayoutProps {
   children: React.ReactNode;
 }
 
 export const Layout = ({ children }: PrivateLayoutProps) => {
+  const { location } = useRouterState();
+  const isDetail = /^\/airports\/[^/]+$/.test(location.pathname);
   return (
-    <>
-      <Header />
-      <div className="max-w-[1280px]">{children}</div>
-    </>
+    <div className="w-full min-h-screen flex flex-col bg-white">
+      {!isDetail && <Header />}
+      <div className="flex-1 w-full flex justify-center bg-white">
+        <div className="w-full flex flex-col justify-center items-center">
+          {children}
+        </div>
+      </div>
+    </div>
   );
 };
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,17 +1,12 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
-import { Header } from "../components/layout/Header";
+import { Layout } from "../components/layout/Layout";
 
 export const Route = createRootRoute({
   component: () => (
-    <div className="w-full min-h-screen flex flex-col bg-white">
-      <Header />
-      <div className="flex-1 w-full flex justify-center bg-white">
-        <div className="w-full flex flex-col justify-center items-center">
-          <Outlet />
-        </div>
-      </div>
+    <Layout>
+      <Outlet />
       <TanStackRouterDevtools />
-    </div>
+    </Layout>
   ),
 });
